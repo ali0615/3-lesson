9cb2df14f4409aaf5ca4e5667334ee9892ca66d1
ali0615 <aliminullin31@gmail.com>
2025-01-31 16:25:48 +0500

    asdfsadf

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let openmodal = function() {
+document.querySelector ("modal").classList.add
+("active-modal");
+}
+const closemodal = () => {
+    document.querySelector ("modal").classList.remove
+    ("active-modal"); 
+}
